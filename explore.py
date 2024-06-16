a8e95391652829d11f0d11c5ad9429d6f84e5b47
Hellen-Adua <155617576+Hellen-Adua@users.noreply.github.com>
2024-06-17 01:01:19 +0300

    Update explore.py

--- explore.py
+++ explore.py
@@ -10,25 +10,24 @@ class DataExplorer:
     def print_statistical_summary(self):
         title = "Statistical Summary of dataset"
         summary = self.data.describe().transpose()
-        print(title)
-        print(summary)
+        return title, summary
 
     def print_dataset_information(self):
         title = "A brief information about the data set"
         info = self.data.info()
-        print(title)
-        print(info)    
+        st.title(title)
+        st.table(info)    
 
     def print_dataframe_shape(self):
         title = "Shape of the data"
         shape = self.data.shape
-        print(title)
+        st.title(title)
         print(shape)
 
     def print_head_of_data(self, size = 5):
         title = "Head of the data"
         head  = self.data.head(size)
-        print(title)
+        st.title(title)
         print(head)     
 
     def print_tail_of_data(self, size =5):     
@@ -40,25 +39,25 @@ class DataExplorer:
     def print_null_values_count(self):
         title = "Count of null values: "
         null_count  = self.data.isnull().sum()
-        print(title)
+        st.title(title)
         print(null_count)  
 
     def print_duplicated_values_count(self):
         title = "Number of duplicated values: "
         duplicates  = self.data.duplicated().sum()
-        print(title)
+        st.title(title)
         print(duplicates)
 
     def print_unique_values_count(self):
         title = "Count of unique values per feature"
         unique_couts  = self.data.nunique()
-        print(title)
+        st.title(title)
         print(unique_couts)  
 
     def print_value_counts(self, category):
         title = "Value counts per category"
         counts = self.data[category].value_counts()
-        print(title)
+        st.title(title)
         print(counts)
 
     def correlation(self):
