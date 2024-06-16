eb556ddc1d7ee40a1322bf4926fd156fb4458c15
Hellen-Adua <155617576+Hellen-Adua@users.noreply.github.com>
2024-06-17 02:37:53 +0300

    Update supervised_models.py

--- supervised_models.py
+++ supervised_models.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import steamlit as st
 
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
