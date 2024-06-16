6ab520e995c1d790d70e27d558b5f8c27b19fc52
Hellen-Adua <155617576+Hellen-Adua@users.noreply.github.com>
2024-06-17 01:30:33 +0300

    Update explore.py

--- explore.py
+++ explore.py
@@ -17,27 +17,9 @@ class DataExplorer:
 
     def print_dataset_information(self):
         title = "A brief information about the data set"
-        
-        # info_df = self.data.info(verbose=False, memory_usage='deep')
-
-        # Convert the info DataFrame to a table format
-        
-        # info_table = info_df.reset_index()
-        # info_table.columns = ['Column', 'Non-Null Count', 'Dtype']
-        # info_table['Column'] = info_table['Column'].str.replace(' ', '')
-
-        # Display the info table using st.table()
-        # st.write(info_df)
-        
-        # buffer = io.StringIO()
-        # self.data.info(buf=buffer)
-        # info_str = buffer.getvalue()
-        # return info_str
-        # # info = self.data.info()
         st.title(title)
-        # # return self.data.info()
-        
-        # Capture df.info() output as a string
+              
+        # Capture data.info() output as a string
         info_str = io.StringIO()
         self.data.info(buf=info_str)
         info_str = info_str.getvalue()
@@ -58,7 +40,7 @@ class DataExplorer:
         title = "Shape of the data"
         shape = self.data.shape
         st.title(title)
-        print(shape)
+        st.write(shape)
 
     def print_head_of_data(self, size = 5):
         title = "Head of the data"
